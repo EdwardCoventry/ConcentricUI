bb1b7926012d66d477a1de55b16b40c42dee92f8
EdwardCoventry <EddCoventry2@gmail.com>
2020-02-16 15:03:24 +0000

    version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ConcentricUI',
-    version='0.1.2',
+    version='0.1.3',
     packages=['ConcentricUI',
               'ConcentricUI.pseudoplyer',
               'ConcentricUI.circle',
